ef850c327e95db6dc639da84b2821ca058473938
Getenet Demsie <demsie_getenet@yahoo.com>
2020-03-23 09:34:43 -0500

    Updated PPB color scale & pop up information

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -215,11 +215,12 @@ export default {
         },
         getMarkerColor(sensor) {
             var PM = Number(sensor[this.pmType]);
-            if(PM >= 0   && PM <=5)   return "#ffff66";
-            else if(PM >  5   && PM <=10)  return "#ff6600";
-            else if(PM > 10   && PM <=15)  return "#cc0000";
-            else if(PM > 15   && PM <=20)  return "#990099";
-            else if(PM > 20) return "#732626";
+            var PPB = 1*PM;                        
+                 if(PPB >= 0    && PPB <=25)   return "#ffff66";
+            else if(PPB > 25    && PPB <=50)   return "#ff6600";
+            else if(PPB > 50    && PPB <=100)  return "#cc0000";
+            else if(PPB > 100   && PPB <=150)  return "#990099";
+            else if(PPB > 150)                 return "#732626";
         },
         getSVGMarker(color) {
             var svg = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30pt" height="32pt" viewBox="0 0 30 32" version="1.1">
