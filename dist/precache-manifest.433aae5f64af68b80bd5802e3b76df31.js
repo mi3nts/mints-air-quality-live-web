e2955ec117be74f754faf36d397305776290828f
kimjulia1117 <kimjulia1117@gmail.com>
2020-04-03 08:42:23 -0500

    Remove PM1 and PM10 options since they do not correlate with the color scheme for PM2.5

---
+++ dist/precache-manifest.433aae5f64af68b80bd5802e3b76df31.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daa40d7d793494c17990",
+    "url": "/css/app.f625af0e.css"
+  },
+  {
+    "revision": "ab1106d1066ebd287342",
+    "url": "/css/chunk-vendors.6ed182d8.css"
+  },
+  {
+    "revision": "6acc66a317d8a1dc54b54cecee3ba2e1",
+    "url": "/img/colorscale.png"
+  },
+  {
+    "revision": "bdad90647b43ffc58ba0e6285c2a333d",
+    "url": "/img/colorscale1.png"
+  },
+  {
+    "revision": "b8860cefae40e3e1f873c647bfef784e",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "e47eab661979cd62ef0c7d956d51cc51",
+    "url": "/img/logo_white.png"
+  },
+  {
+    "revision": "0dd2f073c6bccde9baf2a8f738077c25",
+    "url": "/img/marker.png"
+  },
+  {
+    "revision": "275e5ae274764f558c745539e3c419f2",
+    "url": "/img/newcolorscale.png"
+  },
+  {
+    "revision": "089c623121464d14c9265659764118d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daa40d7d793494c17990",
+    "url": "/js/app.15e1bc7c.js"
+  },
+  {
+    "revision": "ab1106d1066ebd287342",
+    "url": "/js/chunk-vendors.d6c65d93.js"
+  },
+  {
+    "revision": "5f1a8d92a0e70ea9086fc50d49b25af3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "e699ff9a56082f0ea68b41fb66398b13",
+    "url": "/package.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8adfb768dfa3a43568c3d46b1954b517",
+    "url": "/server.js"
+  },
+  {
+    "revision": "bf14962b677feba89a8f1d4a7a788ade",
+    "url": "/web.config"
+  }
+]);
