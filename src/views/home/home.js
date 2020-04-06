ece5aeed5c11307229a7e644f3bdf89ff38cc6d5
Incephalon <incephalon@gmail.com>
2020-04-06 15:58:25 +0530

    radar & wind as layers, all layers with toggle functionality, wind layer customization

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -24,19 +24,55 @@ export default {
             windControl: null,
             radarLayer: false,
             windLayer: false,
+            sensorLayer: true,
+            epaLayer: false,
+            purpleAirLayer: false,
+            openAQLayer: false,
             /** Popup controls */
             howToUse: false,
             /** Currently selected PM type */
             pmType: "pm2_5",
             /** Default state of left side expansion panels */
-            panels: [1, 1, 0, 0],
+            activePanel: 0,
             /** All available sensor instances  */
-            sensors: []
+            sensors: [],
+            sensorGroup: L.layerGroup(),
+            openAQGroup: L.layerGroup(),
+            purpleAirGroup: L.layerGroup(),
+            epaGroup: L.layerGroup(),
         }
     },
     watch: {
         'pmType': function () {
             this.refreshIcons()
+        },
+        'openAQLayer': function (newValue) {
+            if (newValue) {
+                this.openAQGroup.addTo(this.map)
+            } else {
+                this.map.removeLayer(this.openAQGroup);
+            }
+        },
+        'purpleAirLayer': function (newValue) {
+            if (newValue) {
+                this.purpleAirGroup.addTo(this.map)
+            } else {
+                this.map.removeLayer(this.purpleAirGroup);
+            }
+        },
+        'epaLayer': function (newValue) {
+            if (newValue) {
+                this.epaGroup.addTo(this.map)
+            } else {
+                this.map.removeLayer(this.epaGroup);
+            }
+        },
+        'sensorLayer': function (newValue) {
+            if (newValue) {
+                this.sensorGroup.addTo(this.map)
+            } else {
+                this.map.removeLayer(this.sensorGroup);
+            }
         },
         'radarLayer': function (newValue) {
             if (newValue) {
@@ -127,7 +163,7 @@ export default {
             this.buildWindControl();
 
             /** Wind Layer */
-            this.buildWindLayer();
+            this.buildWindLayer('Carto Positron');
         },
         buildWindControl: function () {
             L.Control.WindControl = L.Control.extend({
@@ -147,7 +183,53 @@ export default {
                 }
             });
         },
-        buildWindLayer: function () {
+        windColorScale: function (layerName) {
+            var dark = [
+                "rgb(36,104, 180)",
+                "rgb(60,157, 194)",
+                "rgb(128,205,193 )",
+                "rgb(151,218,168 )",
+                "rgb(198,231,181)",
+                "rgb(238,247,217)",
+                "rgb(255,238,159)",
+                "rgb(252,217,125)",
+                "rgb(255,182,100)",
+                "rgb(252,150,75)",
+                "rgb(250,112,52)",
+                "rgb(245,64,32)",
+                "rgb(237,45,28)",
+                "rgb(220,24,32)",
+                "rgb(180,0,35)"
+            ];
+
+            var light = [
+                "rgb(219,151,75)",
+                "rgb(195,98,61)",
+                "rgb(127,50,62)",
+                "rgb(104,37,87)",
+                "rgb(57,24,87)",
+                "rgb(17,8,38)",
+                "rgb(0,17,38)",
+                "rgb(3,38,130)",
+                "rgb(0,73,155)",
+                "rgb(3,105,180)",
+                "rgb(5,143,203)",
+                "rgb(10,191,223)",
+                "rgb(18,210,227)",
+                "rgb(35,231,223)",
+                "rgb(75,255,220)"
+            ];
+
+
+            if (layerName == "Dark Mode" || layerName == "Satellite") {
+                console.log("Dark");
+                return dark;
+            } else {
+                console.log("Light");
+                return light;
+            }
+        },
+        buildWindLayer: function (layerName, addWhenready) {
             sensorData.getWindData().then(response => {
                 this.layers.wind_layer = L.velocityLayer({
                     displayValues: true,
@@ -162,14 +244,17 @@ export default {
                     },
                     data: response.data,
                     maxVelocity: 10,
+                    colorScale: this.windColorScale(layerName)
                 });
                 this.windControl = new L.Control.WindControl({
                     position: 'bottomright',
                     data_time: response.data[0].recorded_time.replace(".000Z", ""),
                     updated_time: response.data[0].header.refTime.replace(".000Z", "")
                 });
+                if (addWhenready) {
+                    this.windLayer = true;
+                }
             });
-
         },
         initMap: function () {
             this.map = L.map('map', {
@@ -184,13 +269,21 @@ export default {
             }));
             this.map.doubleClickZoom.disable();
             this.layerControl.addTo(this.map);
+            this.sensorGroup.addTo(this.map);
+            this.map.on('baselayerchange', (event) => {
+                console.log(event);
+                if (this.windLayer) {
+                    this.windLayer = false;
+                    this.buildWindLayer(event.name, true);
+                }
+            });
         },
         loadData: function () {
             sensorData.getSensors().then(response => {
                 response.data.forEach(s => {
                     sensorData.getSensorData(s).then(sensorResponse => {
                         sensorResponse.data.id = s;
-                       this.sensors.push(sensorResponse.data[0]);
+                        this.sensors.push(sensorResponse.data[0]);
                         this.renderSensor(sensorResponse.data[0]);
                     });
                 });
@@ -200,37 +293,36 @@ export default {
         // single click pop up information
         renderSensor: function (sensor) {
             let PopupString = "<div style='font-size:14px'><div style='text-align:center; font-weight:bold'>" + "Current Sensor Data </div><br>";
-    if(!isNaN(parseFloat(sensor.pm1)) && parseFloat(sensor.pm1) !== 0)
-      PopupString += "<li>PM1: " + parseFloat(sensor.pm1).toFixed(2) + " Micrograms Per Cubic Meter</li><br>";
-    if(!isNaN(parseFloat(sensor.pm2_5)) && parseFloat(sensor.pm2_5) !== 0)
-      PopupString += "<li>PM2.5: " + parseFloat(sensor.pm2_5).toFixed(2) + " Micrograms Per Cubic Meter</li><br>" ;
-    if(!isNaN(parseFloat(sensor.pm4)) && parseFloat(sensor.pm4) !== 0)
-      PopupString += "<li>PM4: " + parseFloat(sensor.pm4).toFixed(2) + " Micrograms Per Cubic Meter</li><br>" ;
-    if(!isNaN(parseFloat(sensor.pm10))&& parseFloat(sensor.pm10) !== 0)    
-      PopupString += "<li>PM10: " + parseFloat(sensor.pm10).toFixed(2) + " Micrograms Per Cubic Meter</li><br>" ;
-    if(!isNaN(parseFloat(sensor.temperature))&& parseFloat(sensor.temperature) !== 0)
-      PopupString += "<li>Temperature: " + parseFloat(sensor.temperature).toFixed(2) + " Celcius</li><br>" ;
-    if(!isNaN(parseFloat(sensor.humidity))&& parseFloat(sensor.humidity) !== 0)
-      PopupString += "<li>Humidity: " + parseFloat(sensor.humidity).toFixed(2) + "%</li><br>" ;
-    if(!isNaN(parseFloat(sensor.dewpoint))&& parseFloat(sensor.dewpoint) !== 0)
-      PopupString += "<li>DewPoint: " + parseFloat(sensor.dewpoint).toFixed(2) + "%</li></div><br>" 
-    if(!isNaN(parseFloat(sensor.timestamp)))
-      PopupString += "<div style='text-align:right; font-size: 11px'>Last Updated: " + sensor.timestamp + " UTC</div>";
-          
+            if (!isNaN(parseFloat(sensor.pm1)) && parseFloat(sensor.pm1) !== 0)
+                PopupString += "<li>PM1: " + parseFloat(sensor.pm1).toFixed(2) + " Micrograms Per Cubic Meter</li><br>";
+            if (!isNaN(parseFloat(sensor.pm2_5)) && parseFloat(sensor.pm2_5) !== 0)
+                PopupString += "<li>PM2.5: " + parseFloat(sensor.pm2_5).toFixed(2) + " Micrograms Per Cubic Meter</li><br>";
+            if (!isNaN(parseFloat(sensor.pm4)) && parseFloat(sensor.pm4) !== 0)
+                PopupString += "<li>PM4: " + parseFloat(sensor.pm4).toFixed(2) + " Micrograms Per Cubic Meter</li><br>";
+            if (!isNaN(parseFloat(sensor.pm10)) && parseFloat(sensor.pm10) !== 0)
+                PopupString += "<li>PM10: " + parseFloat(sensor.pm10).toFixed(2) + " Micrograms Per Cubic Meter</li><br>";
+            if (!isNaN(parseFloat(sensor.temperature)) && parseFloat(sensor.temperature) !== 0)
+                PopupString += "<li>Temperature: " + parseFloat(sensor.temperature).toFixed(2) + " Celcius</li><br>";
+            if (!isNaN(parseFloat(sensor.humidity)) && parseFloat(sensor.humidity) !== 0)
+                PopupString += "<li>Humidity: " + parseFloat(sensor.humidity).toFixed(2) + "%</li><br>";
+            if (!isNaN(parseFloat(sensor.dewpoint)) && parseFloat(sensor.dewpoint) !== 0)
+                PopupString += "<li>DewPoint: " + parseFloat(sensor.dewpoint).toFixed(2) + "%</li></div><br>"
+            if (!isNaN(parseFloat(sensor.timestamp)))
+                PopupString += "<div style='text-align:right; font-size: 11px'>Last Updated: " + sensor.timestamp + " UTC</div>";
+
             sensor.marker = L.circleMarker([sensor.latitude, sensor.longitude], {
-                fillColor : this.getMarkerColor(sensor),
-                fillOpacity : 0.8,
-                color : "#38b5e6"
+                fillColor: this.getMarkerColor(sensor),
+                fillOpacity: 0.8,
+                color: "#38b5e6"
             });
 
             //handles click event for single click events
-            sensor.marker.addTo(this.map);
+            sensor.marker.addTo(this.sensorGroup);
             sensor.marker.on('click', () => {
                 this.selectedSensor = sensor;
-            }).bindPopup (PopupString);
-            this.markers.push(sensor);
+            }).bindPopup(PopupString);
         },
-        
+
         buildMarkerIcon: function (sensor) {
             /** If you change SCG marker,
              *  you need to fine tune  iconAnchor, iconSize & popupAnchor as well*/
@@ -242,10 +334,12 @@ export default {
                 popupAnchor: [0, -30]
             })
         },
-       
+
         refreshIcons() {
             this.sensors.forEach(sensor => {
-                sensor.marker.setStyle({fillColor : this.getMarkerColor(sensor)});
+                sensor.marker.setStyle({
+                    fillColor: this.getMarkerColor(sensor)
+                });
             });
         },
         getMarkerColor(sensor) {
