be151e62dae7172f6365c450af417b321f813462
giakhanh780f <john1909d@gmail.com>
2020-06-08 13:19:53 -0500

    Increased maximum zoom level (applies to the Carto Position map only)

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -133,7 +133,7 @@ export default {
             this.layers.bright = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
                 subdomains: 'abcd',
-                maxZoom: 19
+                maxZoom: 23
             });
             this.layerControl.addBaseLayer(this.layers.bright, "Carto Positron");
 
