d339a96d969eaafbd5d6238bd95d74c435330c25
dev78080d-desktop <780dev@gmail.com>
2020-07-21 16:42:35 -0500

    Updated API link to use HTTPS

--- src/services/sensor-data.js
+++ src/services/sensor-data.js
@@ -6,7 +6,7 @@ import Vue from "vue";
 export default new Vue({
     data: function () {
         return {
-            baseUrl: "http://mintsdata.utdallas.edu:3000"
+            baseUrl: "https://mintsdata.utdallas.edu:3000"
         }
     },
     methods: {
