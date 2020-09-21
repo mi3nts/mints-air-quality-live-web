cb4ce0cf3f777367fc0f2183e04b5d482df30a82
bry88002 <bryanthfung88@gmail.com>
2020-09-20 22:59:20 -0500

    Sensor Chart Improvements
    #28
    -Color improvements
    -added bolder PM_2_5 line
    -added area Shading between the deviations

--- src/components/sensor-chart/sensor-chart.js
+++ src/components/sensor-chart/sensor-chart.js
@@ -1,3 +1,4 @@
+
 import sensorData from "../../services/sensor-data";
 
 export default {
@@ -7,7 +8,7 @@ export default {
         endDateModel: false,
         startDate: null,
         endDate: null,
-        dataInterval: '', 
+        dataInterval: '',
         switchText: "Show Hourly Averages",
         viewHourly: false
     }),
@@ -29,9 +30,9 @@ export default {
             $("#chart2_" + this.sensor.sensor_id).html("<div class='my-4 text-center'>Loading data...</div>");
             sensorData.getChartData(
                 this.sensor.sensor_id, {
-                    start: this.$moment.utc(this.startDate).toISOString(),
-                    end: this.$moment.utc(this.endDate).toISOString(),
-                }, this.dataInterval
+                start: this.$moment.utc(this.startDate).toISOString(),
+                end: this.$moment.utc(this.endDate).toISOString(),
+            }, this.dataInterval
             ).then(response => {
                 if (response.data.length) {
                     $("#chart2_" + this.sensor.sensor_id).html("<svg> </svg>")
@@ -44,7 +45,7 @@ export default {
         updateDataInterval: function (value) {
             this.dataInterval = value
             var text = 'All data'
-            switch(value) {
+            switch (value) {
                 case '1 60:60:60':
                     text = '1 Day'
                     break
@@ -64,33 +65,33 @@ export default {
             var length = values.length;
             length = length / 3000;
             length = Math.round(length);
-            
+
             var newValues = [];
             var temp = [];
             var indexJ = 0;
             var average = 0;
-            
-            temp.push({x:values[0].x, y:values[0].y});
-            for(var i = 1; i < values.length; i++){
-                if(temp.length >= length){
+
+            temp.push({ x: values[0].x, y: values[0].y });
+            for (var i = 1; i < values.length; i++) {
+                if (temp.length >= length) {
                     average = 0;
-                    for(indexJ = 0; indexJ < temp.length; indexJ++){
+                    for (indexJ = 0; indexJ < temp.length; indexJ++) {
                         average += temp[indexJ].y;
                     }
                     average = average / temp.length;
-                    newValues.push({x:temp[0].x, y:average});
-                    
+                    newValues.push({ x: temp[0].x, y: average });
+
                     temp = [];
                 }
-                temp.push({x:values[i].x, y:values[i].y});
+                temp.push({ x: values[i].x, y: values[i].y });
             }
-            if(temp.length >= 1){
+            if (temp.length >= 1) {
                 average = 0;
-                for(indexJ = 0; indexJ < temp.length; indexJ++){
+                for (indexJ = 0; indexJ < temp.length; indexJ++) {
                     average += temp[indexJ].y;
                 }
                 average = average / temp.length;
-                newValues.push({x:temp[0].x, y:average});
+                newValues.push({ x: temp[0].x, y: average });
             }
             return newValues;
         },
@@ -99,7 +100,7 @@ export default {
             var sensorValues = [];
             var standardDevPos = [];
             var standardDevNeg = [];
-            if(!this.viewHourly) {
+            if (!this.viewHourly) {
                 for (var i = 0; i < data.length; i++) {
                     sensorValues.push({
                         x: this.$moment.utc(data[i].timestamp).local().toDate(),
@@ -116,7 +117,7 @@ export default {
 
                 for (var j = 0; j < data.length; j++) {
                     currHour = this.$moment.utc(data[j].timestamp).local().toDate().getHours();
-                    
+
                     if (currHour == prevHour || prevHour == null) {
                         hourlyValues.push(data[j].pm2_5);
                     } else {
@@ -141,7 +142,7 @@ export default {
                             x: this.$moment.utc(data[prevHourStart].timestamp).local().toDate(),
                             y: avg + sd
                         })
-                        
+
                         // set marker index for next hour to display
                         prevHourStart = j;
 
@@ -151,17 +152,17 @@ export default {
 
                     prevHour = currHour;
                 }
-            }        
-            if(sensorValues.length > 3000){
+            }
+            if (sensorValues.length > 3000) {
                 console.log("changeInterval is called...");
                 sensorValues = this.changeInterval(sensorValues);
             }
             var maxYValue;
             if (this.viewHourly) {
-                maxYValue = Math.max.apply(Math, standardDevPos.map(function(o) { return o.y; }))
+                maxYValue = Math.max.apply(Math, standardDevPos.map(function (o) { return o.y; }))
             } else {
-                maxYValue = Math.max.apply(Math, sensorValues.map(function(o) { return o.y; }))
-            }   
+                maxYValue = Math.max.apply(Math, sensorValues.map(function (o) { return o.y; }))
+            }
 
             var yellowValue = 0;
             var orangeValue = 0;
@@ -189,11 +190,43 @@ export default {
                 purpleValue = 50;
                 maroonValue = maxYValue - (yellowValue + orangeValue + redValue + purpleValue);
             }
+            //make data set for shading
+            var areaDataBottom = [];
+            if (!standardDevPos.length == 0) {
+                for (var x = 0; x < sensorValues.length; x++) {
+                    areaDataBottom.push({
+                        x: sensorValues[x].x,
+                        y: standardDevNeg[x].y
+                    })
+                }
+            }
+            var areaDataMiddle = [];
+            if (!standardDevPos.length == 0) {
+                for (x = 0; x < sensorValues.length; x++) {
+                    areaDataMiddle.push({
+                        x: sensorValues[x].x,
+                        y: sensorValues[x].y - standardDevNeg[x].y
+                    })
+                }
+            }
+            var areaDataTop = [];
+            if (!standardDevPos.length == 0) {
+                for (x = 0; x < sensorValues.length; x++) {
+                    areaDataTop.push({
+                        x: sensorValues[x].x,
+                        y: standardDevPos[x].y - sensorValues[x].y
+                    })
+                }
+            }
+
+
+
             var chartData = [
                 //data
                 {
                     key: "PM 2.5",
-                    values: [{}]
+                    values: [{}],
+                    strokeWidth: this.viewHourly ? 5 : 1.5,
                 },
                 { // standard deviation (-)
                     key: "PM 2.5 -SD",
@@ -205,73 +238,89 @@ export default {
                     values: [{}],
                     color: "#427E24",
                 },
-                
+
                 // color ranges of µg/m³
                 { //0-10µg/m³ yellow
                     key: "0-10µg/m³",
                     values: [{
-                            x: sensorValues[0].x,
-                            y: yellowValue
-                        },
-                        {
-                            x: sensorValues[sensorValues.length - 1].x,
-                            y: yellowValue
-                        }
+                        x: sensorValues[0].x,
+                        y: yellowValue
+                    },
+                    {
+                        x: sensorValues[sensorValues.length - 1].x,
+                        y: yellowValue
+                    }
                     ],
                     color: '#ffff44'
                 },
                 { //10-20/m³ orange
                     key: "10-20µg/m³",
                     values: [{
-                            x: sensorValues[0].x,
-                            y: orangeValue
-                        },
-                        {
-                            x: sensorValues[sensorValues.length - 1].x,
-                            y: orangeValue
-                        }
+                        x: sensorValues[0].x,
+                        y: orangeValue
+                    },
+                    {
+                        x: sensorValues[sensorValues.length - 1].x,
+                        y: orangeValue
+                    }
                     ],
                     color: '#ff5500'
                 },
                 { //20-50µg/m³ red
                     key: "20-50µg/m³",
                     values: [{
-                            x: sensorValues[0].x,
-                            y: redValue
-                        },
-                        {
-                            x: sensorValues[sensorValues.length - 1].x,
-                            y: redValue
-                        }
+                        x: sensorValues[0].x,
+                        y: redValue
+                    },
+                    {
+                        x: sensorValues[sensorValues.length - 1].x,
+                        y: redValue
+                    }
                     ],
                     color: '#cc0000'
                 },
                 { //50-100µg/m³ purple
                     key: "50-100µg/m³",
                     values: [{
-                            x: sensorValues[0].x,
-                            y: purpleValue
-                        },
-                        {
-                            x: sensorValues[sensorValues.length - 1].x,
-                            y: purpleValue
-                        }
+                        x: sensorValues[0].x,
+                        y: purpleValue
+                    },
+                    {
+                        x: sensorValues[sensorValues.length - 1].x,
+                        y: purpleValue
+                    }
                     ],
                     color: '#990099'
                 },
                 { //100+µg/m³ maroon
                     key: "100+µg/m³",
                     values: [{
-                            x: sensorValues[0].x,
-                            y: maroonValue
-                        },
-                        {
-                            x: sensorValues[sensorValues.length - 1].x,
-                            y: maroonValue
-                        }
+                        x: sensorValues[0].x,
+                        y: maroonValue
+                    },
+                    {
+                        x: sensorValues[sensorValues.length - 1].x,
+                        y: maroonValue
+                    }
                     ],
                     color: '#aa2626'
+                },
+                {
+                    key: "SD Area Bottom",
+                    values: areaDataBottom,
+                    color: 'rgba(255, 255, 68,0)',
+                },
+                {
+                    key: "SD Area Middle",
+                    values: areaDataMiddle,
+                    color: "rgb(31, 119, 180)",
+                },
+                {
+                    key: "SD Area Top",
+                    values: areaDataTop,
+                    color: "#F8C630",
                 }
+
             ];
 
             //sets the chart types among other things
@@ -296,18 +345,32 @@ export default {
             chartData[7].type = "area";
             chartData[7].yAxis = 1;
 
+            //second shart above for area shading
+            chartData[8].type = "area";
+            chartData[8].yAxis = 2;
+            chartData[9].type = "area";
+            chartData[9].yAxis = 2;
+            chartData[10].type = "area";
+            chartData[10].yAxis = 2;
+
+
+
+
+
             let hourlyTicks = this.viewHourly
             var sensor_id_chart = this.sensor.sensor_id
-            nv.addGraph(function () {   
+            nv.addGraph(function () {
                 var chart = nv.models.multiChart()
                     .margin({
-                        top: 50,
+                        top: 85,
                         right: 60,
                         bottom: 50,
                         left: 90
                     })
                     .color(d3.scale.category10().range())
                     .yDomain1([0, maxYValue]);
+
+                chart.yDomain2([0, maxYValue])
                 chart.legend.updateState(false);
                 chart.xAxis
                     .tickFormat(function (d) {
@@ -322,11 +385,20 @@ export default {
                     .tickFormat(function (d) {
                         return d3.format(',.2f')(d) + 'µg/m³'
                     });
+                chart.yAxis2
+                    .tickFormat(function (d) {
+                        return d3.format(',.2f')(d) + 'µg/m³'
+                    });
+
+
                 d3.select("#chart2_" + sensor_id_chart + " svg")
                     .datum(chartData)
                     .transition()
                     .duration(500)
                     .call(chart);
+
+
+
                 return chart;
             });
         }
