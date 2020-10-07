1632e0e35ef63ab511c01edb67adf0468085d28b
Robert Wu <robertwu9072@gmail.com>
2020-10-07 15:56:52 -0500

    3

--- src/views/dashboard/dashboard.js
+++ src/views/dashboard/dashboard.js
@@ -1,5 +1,6 @@
+import echarts from "echarts"
 export default {
-    data: () => ({
+    /*data: () => ({
         chartOptionsLine: {
             xAxis: {
                 data: [
@@ -33,5 +34,45 @@ export default {
             },
             color: ["#127ac2"]
         }
-    })
+    }),*/
+    mounted: function() {
+        var chartOptionsLine = {
+            xAxis: {
+                data: [
+                    "Jan",
+                    "Feb",
+                    "Mar",
+                    "Apr",
+                    "May",
+                    "Jun",
+                    "Jul",
+                    "Aug",
+                    "Sep",
+                    "Oct",
+                    "Nov",
+                    "Dec"
+                ]
+            },
+            yAxis: {
+                type: "value"
+            },
+            series: [{
+                type: "line",
+                data: [55, 72, 84, 48, 59, 62, 87, 75, 94, 101, 127, 118]
+            }],
+            title: {
+                text: "Monthly Stock Prices",
+                x: "center",
+                textStyle: {
+                    fontSize: 24
+                }
+            },
+            color: ["#127ac2"]
+        };
+        this.echartInstance = echarts.init(document.getElementsByClassName('charts'));
+        this.echartInstance.setOption(chartOptionsLine)
+        window.addEventListener("resize", function() {
+            this.echartInstance.resize()
+        })
+    }
 };
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,3 +1,66 @@
-<template src="./dashboard.html"></template>
-<script src="./dashboard.js"></script>
-<style src="./dashboard.scss" scoped lang="scss"></style>
+<template>
+    <div>
+        <div id="chart-container"></div>
+    </div>
+</template>
+ 
+<script>
+import echarts from 'echarts'
+ 
+export default {
+    mounted: function() {
+        var chartOptionsLine = {
+            xAxis: {
+                data: [
+                    "Jan",
+                    "Feb",
+                    "Mar",
+                    "Apr",
+                    "May",
+                    "Jun",
+                    "Jul",
+                    "Aug",
+                    "Sep",
+                    "Oct",
+                    "Nov",
+                    "Dec"
+                ]
+            },
+            yAxis: {
+                type: "value"
+            },
+            series: [{
+                type: "line",
+                data: [55, 72, 84, 48, 59, 62, 87, 75, 94, 101, 127, 118]
+            }],
+            title: {
+                text: "Monthly Stock Prices",
+                x: "center",
+                textStyle: {
+                    fontSize: 24
+                }
+            },
+            color: ["#127ac2"]
+        };
+        this.echartInstance = echarts.init(document.getElementById('chart-container'));
+        this.echartInstance.setOption(chartOptionsLine);
+        window.addEventListener("resize",this.resizeHandle);
+    },
+    methods:{
+        resizeHandle(){
+            this.echartInstance.resize()
+        }
+    },
+    destroyed: function() {
+        window.removeEventListener("resize",this.resizeHandle)
+    }
+}
+</script>
+ 
+<style>
+#chart-container{
+    width: 100%;
+    height: 700px;
+}
+</style>
+
