894d23218d93a14f86f6646bd905cc3c1ab46d4c
kangzhiz <kangzhiz@pm.me>
2020-10-12 18:56:09 -0500

    Created live chart updating demo

--- src/components/dashboard-chart/dashboard-chart.js
+++ src/components/dashboard-chart/dashboard-chart.js
@@ -1,64 +1,42 @@
-import ApexCharts from "apexcharts";
+import HighCharts from "highcharts";
 
 export default {
     data: () => ({
         chart: null,
-        sensorValues: [],
-        xTick: 0,
+        sensorValues: [1, 3, 2, 4],
         yTick: 0,
-        tickTimer: null,
     }),
     mounted: function() {
         this.initChart();
     },
     methods: {
         initChart: function() {
-            var options = {
-                series: [{data: this.sensorValues}],
+            this.chart = HighCharts.chart("chart", {
                 chart: {
-                    id: "realtime",
-                    height: 350,
                     type: "line",
-                    animations: {
-                        enabled: true,
-                        easing: "linear",
-                        dynamicAnimations: {
-                            speed: 1000
-                        }
-                    },
-                    toolbar: {
-                        show: false,
-                    },
-                    zoom: {
-                        enabled: false,
-                    },
-                },
-                dataLabels: {
-                    enabled: false,
                 },
                 title: {
                     text: "Test Chart",
-                    alight: "left",
-                },
-                markers: {
-                    size: 0,
-                },
-                xaxis: {
-                    labels: {show: false},
-                    type: "numeric",
-                },
-                yaxis: {
-                    max: 10
                 },
                 legend: {
-                    show: false
+                    enabled: false,
                 },
-            };
-            this.chart = new ApexCharts(document.querySelector("#chart"), options);
-            this.chart.render();
+                plotOptions: {
+                    series: {
+                        pointStart: 0,
+                        marker: {
+                            enabled: false
+                        }
+                    },
+                },
+                series: [{
+                    name: "Test Values",
+                    data: this.sensorValues,
+                }]
+            })
 
             // adds new value every second for demonstration purposes
-            this.tickTimer = window.setInterval(this.addValues, 500);
+            window.setInterval(this.addValues, 500);
         },
         /**
          * Currently alternates between 0 and 7 for demo purposes.
@@ -66,12 +44,6 @@ export default {
          * With MQTT, we should be able to watch for values and
          * then call addValues() to add the data and update 
          * the chart(s).
-         * 
-         * TODO: Fix the bug that shows up when switch back to the map
-         * 
-         * When switching back to map view, the chart attempts to
-         * continue updating in the background. This causes a problem
-         * with the way things are set up currently.
          */
         addValues: function() {
             // alternate between 0 and 7
@@ -82,22 +54,19 @@ export default {
             }
 
             // add item
-            this.sensorValues.push({
-                x: this.xTick,
-                y: this.yTick
-            })
+            this.sensorValues.push(this.yTick)
 
             // remove items once chart gets too full
             if (this.sensorValues.length > 30) {
                 this.sensorValues.shift();
             }
 
-            this.xTick++;
-          
             // update chart
-            this.chart.updateSeries([{
-                data: this.sensorValues
-            }])
+            this.chart.update({
+                series: [{
+                    data: this.sensorValues
+                }]
+            })
         },
     }
 }
