bd71a3b7d00069dc3d1a777dca5c7000a716ae93
kangzhiz <kangzhiz@pm.me>
2020-10-12 23:44:08 -0500

    Reorganized code with separate dashboard-chart component
    Changed demo to match other chart demos

--- src/components/dashboard-chart/dashboard-chart.js
+++ src/components/dashboard-chart/dashboard-chart.js
@@ -1,9 +1,15 @@
-import HighCharts from "highcharts";
+import echarts from "echarts";
 
 export default {
     data: () => ({
         chart: null,
-        sensorValues: [1, 3, 2, 4],
+        sensorValues: [
+            {name: "test", value: [0, 1]},
+            {name: "test", value: [1, 3]},
+            {name: "test", value: [2, 2]},
+            {name: "test", value: [3, 4]}
+        ],
+        xTick: 4,
         yTick: 0,
     }),
     mounted: function() {
@@ -11,29 +17,35 @@ export default {
     },
     methods: {
         initChart: function() {
-            this.chart = HighCharts.chart("chart", {
-                chart: {
-                    type: "line",
-                },
+            var chartOptionsLine = {
                 title: {
-                    text: "Test Chart",
+                    text: "Test Chart"
                 },
-                legend: {
-                    enabled: false,
+                xAxis: {
+                    splitLine: {
+                        show: false
+                    }
                 },
-                plotOptions: {
-                    series: {
-                        pointStart: 0,
-                        marker: {
-                            enabled: false
-                        }
-                    },
+                yAxis: {
+                    type: "value",
+                    boundaryGap: [0, '100%'],
+                    splitLine: {
+                        show: false
+                    }
                 },
                 series: [{
-                    name: "Test Values",
+                    name: 'Test Values',
+                    type: "line",
+                    showSymbol: false,
+                    hoverAnimation: false,
                     data: this.sensorValues,
-                }]
-            })
+                }],
+                color: ["#69b2ee"]
+            };
+
+            this.chart =  echarts.init(document.getElementById("chart"));
+            this.chart.setOption(chartOptionsLine);
+            window.addEventListener("resize", this.resizeHandle);
 
             // adds new value every second for demonstration purposes
             window.setInterval(this.addValues, 500);
@@ -54,19 +66,32 @@ export default {
             }
 
             // add item
-            this.sensorValues.push(this.yTick)
+            this.sensorValues.push({
+                name: "test",
+                value: [this.xTick, this.yTick]
+            });
 
             // remove items once chart gets too full
             if (this.sensorValues.length > 30) {
                 this.sensorValues.shift();
             }
 
+            this.xTick++;
+
             // update chart
-            this.chart.update({
+            this.chart.setOption({
                 series: [{
                     data: this.sensorValues
                 }]
             })
         },
+        resizeHandle: function() {
+            this.chart.resize();
+            //this.echartInstance2.resize();
+            //this.echartInstance3.resize();
+        },
+        destroyed: function() {
+            window.removeEventListener("resize", this.resizeHandle);
+        }
     }
 }
