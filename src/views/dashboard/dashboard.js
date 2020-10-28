70b34a1e7e0af923bec487e37bf590d6ad850eba
kangzhiz <kangzhiz@pm.me>
2020-10-28 15:07:20 -0500

    Fixed merge conflicts switching to real time charts

--- src/views/dashboard/dashboard.js
+++ src/views/dashboard/dashboard.js
@@ -20,64 +20,6 @@ export default {
             chartnames: ["PM2.5", "PM1", "PM10", "Dewpoint", "Humidity", "Pressure", "Temperature"],
         }
     },
-    watch: {
-        'pm2_5': function (newValue) {
-            if (newValue) {
-                $("#test1").css("display", "block");
-            }
-            else {
-                $("#test1").css("display", "none");
-            }
-        },
-        'pm1': function (newValue) {
-            if (newValue) {
-                $("#test2").css("display", "block");
-            }
-            else {
-                $("#test2").css("display", "none");
-            }
-        },
-        'pm10': function (newValue) {
-            if (newValue) {
-                $("#test3").css("display", "block");
-            }
-            else {
-                $("#test3").css("display", "none");
-            }
-        },
-        'dewpoint': function (newValue) {
-            if (newValue) {
-                $("#test4").css("display", "block");
-            }
-            else {
-                $("#test4").css("display", "none");
-            }
-        },
-        'humidity': function (newValue) {
-            if (newValue) {
-                $("#test5").css("display", "block");
-            }
-            else {
-                $("#test5").css("display", "none");
-            }
-        },
-        'pressure': function (newValue) {
-            if (newValue) {
-                $("#test6").css("display", "block");
-            }
-            else {
-                $("#test6").css("display", "none");
-            }
-        },
-        'temperature': function (newValue) {
-            if (newValue) {
-                $("#test7").css("display", "block");
-            }
-            else {
-                $("#test7").css("display", "none");
-            }
-        },
-    },
     mounted: function() {
         // If the page is less than 600px wide, the sidebar starts off hidden
         if ($(window).width() < 600) {
