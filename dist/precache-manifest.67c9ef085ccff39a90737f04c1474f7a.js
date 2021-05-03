1db7f03e4949ebc2fbf6f2e41bc3166c18131bf2
Kevin Flores <42478496+Kevin346@users.noreply.github.com>
2021-05-03 17:32:16 -0500

    GPS Data, Averaged Data, and Now Displays H2O and CO2 Levels

---
+++ dist/precache-manifest.67c9ef085ccff39a90737f04c1474f7a.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00917d94d8ad51d0fb1",
+    "url": "/css/app.a59bd7b2.css"
+  },
+  {
+    "revision": "4992f01c4a52c1fc0c29",
+    "url": "/css/chunk-vendors.3446c5ff.css"
+  },
+  {
+    "revision": "6acc66a317d8a1dc54b54cecee3ba2e1",
+    "url": "/img/colorscale.png"
+  },
+  {
+    "revision": "bdad90647b43ffc58ba0e6285c2a333d",
+    "url": "/img/colorscale1.png"
+  },
+  {
+    "revision": "ab93b5356f3c953703112f6b019e93af",
+    "url": "/img/colorscale2.png"
+  },
+  {
+    "revision": "da1e8503e46fce245501731ea259f912",
+    "url": "/img/graph.PNG"
+  },
+  {
+    "revision": "649260fbf532497e994b63d30e9f2258",
+    "url": "/img/left.png"
+  },
+  {
+    "revision": "b8860cefae40e3e1f873c647bfef784e",
+    "url": "/img/logo.png"
+  },
+  {
+    "revision": "e47eab661979cd62ef0c7d956d51cc51",
+    "url": "/img/logo_white.png"
+  },
+  {
+    "revision": "0dd2f073c6bccde9baf2a8f738077c25",
+    "url": "/img/marker.png"
+  },
+  {
+    "revision": "275e5ae274764f558c745539e3c419f2",
+    "url": "/img/newcolorscale.png"
+  },
+  {
+    "revision": "87be748211989030415a2b171c652402",
+    "url": "/img/north.png"
+  },
+  {
+    "revision": "3a84996c9765c8a32aba60f94869b9f5",
+    "url": "/img/right.png"
+  },
+  {
+    "revision": "f1a7813443cebd8c6f2d2e90bb1af040",
+    "url": "/img/socialshare.PNG"
+  },
+  {
+    "revision": "d7cc9752e19ab540296d3e1db3de8006",
+    "url": "/img/south.png"
+  },
+  {
+    "revision": "3bfb1b21f5fc47d4899c894c70203fad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e00917d94d8ad51d0fb1",
+    "url": "/js/app.e2375c73.js"
+  },
+  {
+    "revision": "4992f01c4a52c1fc0c29",
+    "url": "/js/chunk-vendors.981ecee9.js"
+  },
+  {
+    "revision": "d05a8e631733b0979abe53e42468d608",
+    "url": "/json/PollutionBurdenByCouncilDistrict.json"
+  },
+  {
+    "revision": "5f1a8d92a0e70ea9086fc50d49b25af3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "f95cbfdb8887060463fd70b13ec7ed6b",
+    "url": "/package.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "752320090b5e20ec43f20f95dc873333",
+    "url": "/server.js"
+  },
+  {
+    "revision": "bbc18268bbf1a5ab365e6ed89b245bb2",
+    "url": "/web.config"
+  }
+]);
